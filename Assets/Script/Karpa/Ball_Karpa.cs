76941723917e07a582e4437639c1ff46722416de
DESKTOP-V93EF73\keike <keikeita0731@gmail.com>
2024-06-19 00:23:28 +0900

    スクリプト分岐

---
+++ Assets/Script/Karpa/Ball_Karpa.cs
@@ -0,0 +1,83 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+class Ball_Karpa : MonoBehaviour
+{
+    // ボールの移動の速さを指定する変数
+    public float speedBase = 15;
+    private float speed;
+    Rigidbody2D myRigidbody;
+    float angle;
+    public GameObject player;
+    Vector3 m_velocity;
+    int x, y;
+    void Start()
+    {
+        angle = Random.Range(60,120);
+        Application.targetFrameRate = 60; // 30fpsに設定
+        // Rigidbodyにアクセスして変数に保持しておく
+        myRigidbody = GetComponent<Rigidbody2D>();
+        var direction = GetDirection(angle);
+        m_velocity = direction * speed * 0.01f;
+    }
+    //弾の角度
+    public static Vector3 GetDirection(float angle)
+    {
+        return new Vector3
+        (
+            Mathf.Cos(angle * Mathf.Deg2Rad),
+            Mathf.Sin(angle * Mathf.Deg2Rad),0
+        );
+    }
+    //プレイヤーと弾の間の角度
+    public float GetAngleToPlayer()
+    {
+        Vector3 dt = player.transform.position - this.transform.position;
+        float rad = Mathf.Atan2(dt.y, dt.x);
+        float degree = rad * Mathf.Rad2Deg;
+        return degree;
+    }
+    private void Update()
+    {
+        //左Ctrlを押している間加速
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            speed = speedBase*2;
+        }
+        else 
+        {
+            speed = speedBase;
+        }     
+
+        //現在角度を取得
+        var direction = GetDirection(angle);
+        // 発射角度と速さから速度を求める
+        m_velocity = direction * speed * 0.01f;
+        transform.localPosition += m_velocity;
+    }
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Handle collisions based on tag
+        switch (collision.tag)
+        {
+            case "LeftWall"://左端反射
+            case "RightWall"://右端反射
+                angle = 180 - angle;
+                break;
+            case "TopWall"://上端反射
+            case "UnderWall"://下端反射
+                angle = -angle;
+                break;
+            case "Player"://自機に当たって反射
+                angle = GetAngleToPlayer()+180;
+                break;
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/Script/Karpa/Player_Karpa.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+class Player_Karpa : MonoBehaviour
+{
+    // プレイヤーの移動の速さ
+    public float speedBase = 15;
+    private float speed;
+    Rigidbody2D myRigidbody;
+    int x, y;
+    void Start()
+    {
+        // Rigidbodyにアクセスして変数に保持
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 左右のキー入力により速度を変更する
+        myRigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, 0f);
+        Move();
+        if(transform.position.y < -3) 
+        {
+        
+        }
+
+
+    }
+
+    void Move()
+    {
+        x = (int)Input.GetAxisRaw("Horizontal");
+        y = (int)Input.GetAxisRaw("Vertical");
+        //左シフトを押している間は速度低下
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = speedBase/3;
+        }
+        else
+        {
+            speed = speedBase;
+        }
+
+        Vector3 nextPoint = transform.position + new Vector3(x, y, 0) * Time.deltaTime * speed;
+        //Xは-4.2～4.2間でのみ移動可能
+        nextPoint = new Vector3(
+                  Mathf.Clamp(nextPoint.x, -4.2f,  4.2f),
+                  Mathf.Clamp(nextPoint.y, -4f, -4f),
+                  0
+                  );
+
+        transform.position = nextPoint;
+
+    }
+}
